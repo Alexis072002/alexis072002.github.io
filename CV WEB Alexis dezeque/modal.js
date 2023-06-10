709a5e9f1304d6213476c29e870e8b08df791fae
Alexis072002 <134490476+Alexis072002@users.noreply.github.com>
2023-06-10 16:12:23 +0200

    Add files via upload

---
+++ CV WEB Alexis dezeque/modal.js
@@ -0,0 +1,9 @@
+    let modal = document.getElementById("modal");
+    let btn = document.getElementById("btn");
+    let closebtn = document.getElementsByClassName("close-btn")[0];
+    btn.onclick = function() {
+        modal.classList.add("show");
+    }
+    closebtn.onclick = function() {
+        modal.classList.remove("show");
+    }
